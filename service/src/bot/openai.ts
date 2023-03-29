e70404f234f60aa2ce795ad24ae09cb56738a91e
tsubasap91 <tsubasap91@gmail.com>
2023-03-29 11:29:58 +0800

    build test

--- service/src/bot/openai.ts
+++ service/src/bot/openai.ts
@@ -6,26 +6,26 @@ import { ChatMessage, chatReplyProcess } from "src/chatgpt/index.js";
 async function chatgpt(username: string, message: string): Promise<any> {
 
     // 先将用户输入的消息添加到数据库中
-    DBUtils.addUserMessage(username, message);
+    DBUtils.addUserMessage(username, message)
 
     // 查找最后一条消息
-    const messages = DBUtils.getChatMessage(username);
-    let lastMessageIndex = messages && messages.lastIndexOf(e => e.role == ChatCompletionRequestMessageRoleEnum.Assistant);
-    let lastMessage = lastMessageIndex >= 0 ? messages[lastMessageIndex] : null;
+    const messages = DBUtils.getChatMessage(username)
+    let lastMessageIndex = messages && messages.lastIndexOf(e => e.role == ChatCompletionRequestMessageRoleEnum.Assistant)
+    let lastMessage = lastMessageIndex >= 0 ? messages[lastMessageIndex] : null
     let lastContent = lastMessage && {
         conversationId: lastMessage?.conversationId, // 会话id
         parentMessageId: lastMessage?.messageId, // 上一条消息的id
-    };
+    }
 
     // 用户设置的prompt
     const user = DBUtils.getUserByUsername(username);
     let prompt = user.chatMessage.find(
         (msg) => msg.role === ChatCompletionRequestMessageRoleEnum.System
-    )!.content + "";
+    )!.content + ""
 
     // web那边的接口会返回conversionid和messageid
     const response: any = await new Promise(async (res, rej) => {
-        let timer = setTimeout(() => res({ msg: "超时未响应" }), 5000);;
+        let timer = setTimeout(() => res({ msg: "超时未响应" }), 5000)
         try {
             await chatReplyProcess({
                 message: message,
@@ -39,13 +39,13 @@ async function chatgpt(username: string, message: string): Promise<any> {
             })
         } catch (e) {
             console.log("request web api error", e);
-            res({ msg: '请求错误' });
+            res({ msg: '请求错误' })
         }
-    });
+    })
 
     if (response?.id) {
-        response['code'] = 200;
-        return response;
+        response['code'] = 200
+        return response
     } else {
         return { code: -1, msg: response?.msg || "Something went wrong" }
     }
